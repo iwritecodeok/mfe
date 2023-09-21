d4ba8bca78bf840475d4ce7df8c1d58ff906bb77
Justin <nicholsj0621@gmail.com>
2023-09-20 22:17:12 -0500

    initial node-html +node angular

---
+++ Node-Ng-React/host/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static assets
+app.use('/navbar', express.static(path.join(__dirname, '../navbar')));
+app.use('/cta', express.static(path.join(__dirname, '../cta')));
+app.use('/Node-Ng-React/ngFooter/dist/ng-footer/', express.static(path.join(__dirname, '.../ngFooter/dist/ng-footer/')));
+
+app.get('/', (req, res) => {
+    // Read the host HTML
+    const hostHTML = fs.readFileSync(path.join(__dirname, '', 'index.html'), 'utf8');
+    
+    // Read the navbar HTML
+    const navbarHTML = fs.readFileSync(path.join(__dirname, '../navbar', 'index.html'), 'utf8');
+    
+    // Read the CTA HTML
+    const ctaHTML = fs.readFileSync(path.join(__dirname, '../cta', 'index.html'), 'utf8');
+
+    // Combine them together
+    const combinedHTML = hostHTML.replace(
+        '<!-- Navbar will be injected here -->',
+        navbarHTML
+    ).replace(
+        '<!-- CTA will be injected here -->',
+        ctaHTML
+    );
+
+    res.send(combinedHTML);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port https://localhost:${PORT}`);
+});
