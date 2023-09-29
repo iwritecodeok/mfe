5dcb23f3b6afb5a934b1ab998dc3bfb7f863c463
Justin <nicholsj0621@gmail.com>
2023-09-28 23:18:00 -0500

    add-next-app

--- mfe-server/navbar-app/src/App.js
+++ mfe-server/navbar-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRXJswJkP8PeV0T6Ar2jZfNH8cfal12YU",
+  authDomain: "mfe-navbar.firebaseapp.com",
+  projectId: "mfe-navbar",
+  storageBucket: "mfe-navbar.appspot.com",
+  messagingSenderId: "755281463441",
+  appId: "1:755281463441:web:c914c4447b1b4c787c5730"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav">
+      hello
     </div>
   );
 }
